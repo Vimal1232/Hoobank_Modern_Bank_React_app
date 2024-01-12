5c5371e9886008708058aa34b68ab66bfd163785
Vimal Chaudhary <86522515+Vimal1232@users.noreply.github.com>
2024-01-12 14:29:03 +0530

    Hoobank Modern Bank app

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./navbar";
+import Hero from "./hero";
+import Stats from "./Stats";
+import Business from "./business";
+import Billing from "./billing";
+import CardDeal from "./carddeal";
+import Testimonials from "./testimonials";
+import Clients from "./clients";
+import CTA from "./CTA";
+import Footer from "./footer";
+import Getstarted from "./Getstarted";
+import Button from "./button";
+import FeedbackCard from "./feedbackCard";
+
+export {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+  Getstarted,
+  Button,
+  FeedbackCard,
+};
